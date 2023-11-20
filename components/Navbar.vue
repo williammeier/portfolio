c26e00b9e835389af478be532223cc20231dc642
williammeier <william.meier@outlook.com>
2023-11-20 00:40:09 -0300

    remover bg class from nav

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar bg-white dark:bg-dark">
+  <nav class="navbar">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">William</span>
