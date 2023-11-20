9ebecf360f192bdb9f21d56174c2813d5d01c721
williammeier <william.meier@outlook.com>
2023-11-20 00:37:13 -0300

    creating navbar

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Navbar />
-    <p class="p-4 pb-2">
+
+    <div class="p-4 pb-2 mt-5">
       <select
         v-model="colorMode.preference"
         class="border w-24 h-8 dark:bg-gray-900 dark:text-white dark:border-gray-700"
@@ -10,28 +11,6 @@
         <option value="light">Light</option>
         <option value="dark">Dark</option>
       </select>
-    </p>
-
-    <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-      <div>
-        <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-          <svg
-            class="h-6 w-6 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <!-- ... -->
-          </svg>
-        </span>
-      </div>
-      <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-      <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer
-        space.
-      </p>
     </div>
   </div>
 </template>
@@ -44,6 +23,6 @@ const colorMode = useColorMode()
 
 <style lang="postcss">
 body {
-  @apply min-h-screen bg-white dark:bg-gray-800 dark:text-gray-200;
+  @apply min-h-screen bg-white text-dark dark:bg-dark dark:text-white;
 }
 </style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold text-primary underline m-5">Hello world!</h1>
-  </div>
+  <nav class="navbar bg-white dark:bg-dark">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">William</span>
+      </a>
+      <button
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto mx-auto" id="navbar-default">
+        <ul class="flex flex-col p-4 md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse">
+          <li>
+            <a href="#" class="nav-link text-base text-dark dark:text-white" aria-current="page">Home</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-base text-dark dark:text-white">About</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link text-base text-dark dark:text-white">Work</a>
+          </li>
+        </ul>
+      </div>
+      <div></div>
+    </div>
+  </nav>
 </template>
+
+<style lang="scss">
+.navbar {
+  .nav-link {
+    padding: 0.5rem 0.75rem;
+    &:hover {
+      color: theme('colors.primary');
+    }
+  }
+}
+</style>
