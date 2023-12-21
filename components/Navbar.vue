e6fed4e530cff79609fb9510e857ec585e331291
williammeier <william.meier@outlook.com>
2023-12-20 21:23:37 -0300

    ajustes navbar responsiva

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,10 +7,10 @@
           <!-- Logo -->
           <Logo :style="'text-3xl md:hidden text-white'" />
 
-          <div class="navbar-nav mx-auto mt-5 md-mt-0">
-            <ul class="flex flex-col md:p-0 md:flex-row md:space-x-8">
+          <div class="navbar-nav mx-auto mt-7 md:mt-0">
+            <ul class="flex flex-col md:p-0 md:flex-row md:space-x-16">
               <li class="nav-item">
-                <a href="#" class="nav-link text-dark dark:text-white" aria-current="page">Home</a>
+                <a href="#" class="nav-link text-dark dark:text-white">Home</a>
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link text-dark dark:text-white">About</a>
@@ -21,12 +21,12 @@
             </ul>
           </div>
           <ul class="navbar-media flex flex-row align-end mt-4 md:mt-0">
-            <li class="nav-item">
+            <li class="nav-item mr-5">
               <a href="https://github.com/williammeier" target="_blank" title="GitHub" class="nav-link">
                 <font-awesome-icon icon="fa-brands fa-github" />
               </a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mr-5">
               <a href="https://www.linkedin.com/in/williammeier01/" target="_blank" title="Linkedin" class="nav-link">
                 <font-awesome-icon icon="fa-brands fa-linkedin" />
               </a>
@@ -35,17 +35,32 @@
         </div>
 
         <div class="navbar-tools">
-          <button class="nav-link" @click="colorMode.preference = 'dark'" v-if="colorMode.preference === 'light'">
+          <button
+            class="nav-link"
+            @click="colorMode.preference = 'dark'"
+            v-if="colorMode.preference === 'light'"
+            title="Light"
+          >
             <font-awesome-icon icon="fa-solid fa-moon" />
           </button>
-          <button class="nav-link" @click="colorMode.preference = 'light'" v-if="colorMode.preference === 'dark'">
+          <button
+            class="nav-link"
+            @click="colorMode.preference = 'light'"
+            v-if="colorMode.preference === 'dark'"
+            title="Dark"
+          >
             <font-awesome-icon icon="fa-solid fa-sun" />
           </button>
 
-          <button class="nav-link md:hidden" @click="toggleNav">
+          <button class="nav-link md:hidden ml-5" @click="toggleNav" title="Abrir Menu">
             <font-awesome-icon icon="fa-solid fa-bars" />
           </button>
-          <button class="nav-link close md:hidden" :class="!showNav ? 'hidden' : ''" @click="toggleNav">
+          <button
+            class="nav-link close md:hidden"
+            :class="!showNav ? 'hidden' : ''"
+            @click="toggleNav"
+            title="Fechar Menu"
+          >
             <font-awesome-icon icon="fa-solid fa-close" />
           </button>
         </div>
