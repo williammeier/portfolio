c483a5ddee0d5173ffcba4651c51361b888aa69e
williammeier <william.meier@outlook.com>
2023-12-29 23:31:26 -0300

    feito o navbar responsivo para mobile

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,32 +1,31 @@
 <template>
   <div>
     <nav class="navbar">
-      <div class="max-w-screen-xl flex nowrap items-end justify-between mx-auto overflow-hidden py-5">
+      <div class="container mx-auto px-3 md:px-0 flex nowrap items-end justify-between mx-auto py-5">
         <Logo :style="'text-2xl'" />
-        <div class="navbar-colapse" :class="showNav == true ? 'show' : ''">
-          <!-- Logo -->
-          <Logo :style="'text-3xl md:hidden text-white'" />
 
-          <div class="navbar-nav mx-auto mt-7 md:mt-0">
-            <ul class="flex flex-col md:p-0 md:flex-row md:space-x-16">
-              <li class="nav-item">
-                <a href="#" class="nav-link text-dark dark:text-white">Home</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link text-dark dark:text-white">About</a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link text-dark dark:text-white">Work</a>
-              </li>
-            </ul>
-          </div>
-          <ul class="navbar-media flex flex-row align-end mt-4 md:mt-0">
+        <!-- <div class="navbar-background" :class="showNav == true ? 'show' : ''"></div> -->
+
+        <div class="navbar-nav" :class="showNav == true ? 'show' : ''">
+          <!-- <Logo :style="'text-3xl md:hidden text-white'" /> -->
+          <ul class="nav-group mx-auto flex-col md:flex-row md:p-0 md:space-x-16">
+            <li class="nav-item">
+              <a href="#" class="nav-link text-dark dark:text-white">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link text-dark dark:text-white">About</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link text-dark dark:text-white">Work</a>
+            </li>
+          </ul>
+          <ul class="nav-group social flex flex-row align-end mt-4 md:mt-0">
             <li class="nav-item mr-5">
               <a href="https://github.com/williammeier" target="_blank" title="GitHub" class="nav-link">
                 <font-awesome-icon icon="fa-brands fa-github" />
               </a>
             </li>
-            <li class="nav-item mr-5">
+            <li class="nav-item md:mr-5">
               <a href="https://www.linkedin.com/in/williammeier01/" target="_blank" title="Linkedin" class="nav-link">
                 <font-awesome-icon icon="fa-brands fa-linkedin" />
               </a>
@@ -52,17 +51,17 @@
             <font-awesome-icon icon="fa-solid fa-sun" />
           </button>
 
-          <button class="nav-link md:hidden ml-5" @click="toggleNav" title="Abrir Menu">
-            <font-awesome-icon icon="fa-solid fa-bars" />
-          </button>
           <button
-            class="nav-link close md:hidden"
-            :class="!showNav ? 'hidden' : ''"
+            class="nav-toggle md:hidden ml-5"
             @click="toggleNav"
-            title="Fechar Menu"
+            :class="showNav == true ? 'close' : ''"
+            title="Toggle Menu"
           >
-            <font-awesome-icon icon="fa-solid fa-close" />
+            <span class="nav-toggle-icon"></span>
           </button>
+          <!-- <button class="nav-link close md:hidden" @click="toggleNav" title="Fechar Menu">
+            <font-awesome-icon icon="fa-solid fa-close" />
+          </button> -->
         </div>
       </div>
     </nav>
