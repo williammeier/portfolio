b8a30b14d6c6f97fc1baddbe4b2dcad0df16f340
williammeier <william.meier@outlook.com>
2024-01-07 22:45:03 -0300

    remover  badges nas linguagens de programação

--- components/WorkSection.vue
+++ components/WorkSection.vue
@@ -28,46 +28,8 @@
             </p>
           </div>
           <div class="card-stack">
-            <span class="text-base font-normal mr-2">Tech stack:</span>
-            <span class="badge badge-sm badge-danger">HTML</span>
-            <span class="badge badge-sm badge-warning">Java Script</span>
-            <span class="badge badge-sm badge-primary">PHP</span>
-            <span class="badge badge-sm badge-danger">Laravel</span>
-          </div>
-
-          <div class="card-footer">
-            <a href="" class="inline-block rounded text-sm font-semibold hover:text-primary">
-              <font-awesome-icon icon="fa-solid fa-link" />
-              Live Preview
-            </a>
-            <a href="" class="inline-block rounded text-sm font-semibold hover:text-primary">
-              <font-awesome-icon icon="fa-brands fa-github" />
-              View Code
-            </a>
-          </div>
-        </div>
-        <div class="card card-projects">
-          <div class="card-img">
-            <img
-              class="w-full"
-              src="https://images.unsplash.com/photo-1470770903676-69b98201ea1c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div class="card-body">
-            <h2 class="card-title">Project Name</h2>
-
-            <p class="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
-              eaque, exercitationem praesentium nihil.
-            </p>
-          </div>
-          <div class="card-stack">
-            <span class="text-base font-normal mr-2">Tech stack:</span>
-            <span class="badge badge-sm badge-danger">HTML</span>
-            <span class="badge badge-sm badge-warning">Java Script</span>
-            <span class="badge badge-sm badge-primary">PHP</span>
-            <span class="badge badge-sm badge-danger">Laravel</span>
+            <span class="text-base mr-1">Tech stack:</span>
+            HTML, Java Script, PHP, Laravel
           </div>
 
           <div class="card-footer">
