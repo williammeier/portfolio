97e38a2ece39ebc2643787da4fb44cdd80ca3a47
williammeier <william.meier@outlook.com>
2024-01-29 14:28:32 -0300

    add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,28 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'William Dev',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.icon',
+        },
+      ],
+      script: [
+        {
+          innerHTML: `
+            <!-- Google tag (gtag.js) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-LY6DRFQSQX"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-LY6DRFQSQX');
+            </script> 
+          `,
+        },
+      ],
     },
     baseURL: '/portfolio', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
