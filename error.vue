db9b726356e07508b084bec39b17c867b6058512
williammeier <william.meier@outlook.com>
2024-02-11 19:48:52 -0300

    add error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtLayout>
+    <div class="container mx-auto">
+      <div class="error h-screen">
+        <img class="error-img" :src="error.statusCode === 404 ? '/404.svg' : '/error.svg'" alt="404 error" />
+        <h2 class="text-3xl mt-7 mb-10">
+          {{ error.statusCode === 404 ? 'Page not found!' : 'Oops, something went wrong!' }}
+        </h2>
+        <a href="/portfolio" class="btn-primary px-5 py-2 rounded-md">Go back</a>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError()
+</script>
